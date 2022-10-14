228f530432d4ec506c6e12905932f982824197e0
ABC <kawyandun10@gmail.com>
2022-10-15 00:45:15 +0530

    he he

---
+++ src/print.java
@@ -0,0 +1,21 @@
+class MethodOverloading {
+    void display(int a){
+        System.out.println("12345");
+    }
+    void display(int a, double b, double c){
+        System.out.println("123 and abc");
+    }
+    void display(int a, int b){
+        System.out.println("54abcd");
+    }
+    void display(int a, double b){
+        System.out.println("3.89");
+    }
+    public static void main(String[] args) {
+        MethodOverloading mol = new MethodOverloading();
+        mol.display(1);
+        mol.display( 2,4.67,7.87765);
+        mol.display(1);
+        mol.display(1,3.7);
+    }
+}
